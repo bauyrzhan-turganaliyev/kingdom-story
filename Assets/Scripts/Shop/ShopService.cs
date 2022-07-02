73e551110940d71b27f8206e7a467ebdb2943f36
Turganaliyev <turganaliyev@gongulus.net>
2022-07-02 05:59:37 -0700

    Shop selector implemented, fixed bugs with font

---
+++ Assets/Scripts/Shop/ShopService.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace turganaliyev.Shop
+{
+    public class ShopService : MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+        [SerializeField] private GameObject _shopPanel;
+
+        public void OnClickEnter()
+        {
+            _shopPanel.SetActive(true);
+            _transform.SetAsLastSibling();
+        }
+
+        public void OnClickExit()
+        {
+            _shopPanel.SetActive(false);
+        }
+    }
+}
